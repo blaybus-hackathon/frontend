6a3fb753f90588fb42ce7bb869edb61e1e04cf29
TakioN <jungbeen0724@gmail.com>
2025-02-18 08:42:33 +0900

    feat : add matching management page

---
+++ src/pages/MatchingManage2.jsx
@@ -0,0 +1,14 @@
+import Header from '@/components/Header';
+
+export default function MatchingManage2() {
+  return (
+    <>
+      <Header title={'공고 올리기'} />
+      <div className='mx-[1.5rem] flex flex-col items-center'>
+        <p className='font-bold text-xl tracking-[-0.1rem] py-8 w-full mx-auto text-center'>
+          (박순자)어르신의 정보를 확인해주세요
+        </p>
+      </div>
+    </>
+  );
+}

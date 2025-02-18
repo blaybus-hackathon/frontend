4d53180fef54bd77a7b8ae585b0734ab41df8391
bigmooon <bigmooon1215@gmail.com>
2025-02-19 00:59:06 +0900

    test: add test data

--- src/api/index.js
+++ src/api/index.js
@@ -1,12 +1,13 @@
-import axios from "axios";
+import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL; // VITE 환경변수 사용
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://3.37.158.7'; // VITE 환경변수 사용
 
 const api = axios.create({
   baseURL: API_BASE_URL,
   headers: {
-    "Content-Type": "application/json",
+    'Content-Type': 'application/json',
   },
+  withCredentials: true,
 });
 
 export const request = async (method, endpoint, data = {}) => {
@@ -18,7 +19,7 @@ export const request = async (method, endpoint, data = {}) => {
     });
     return response.data;
   } catch (error) {
-    console.error("API 요청 오류: ", error);
+    console.error('API 요청 오류: ', error);
     throw error;
   }
 };

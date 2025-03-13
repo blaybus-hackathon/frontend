e11fbe341e25750c50925d98bffeefb4d34b3574
zhzkal2 <reliya1541@gmail.com>
2025-03-13 18:57:26 +0900

    feat : add story book

--- src/Test.jsx
+++ src/Test.jsx
@@ -1,12 +1,38 @@
 
 
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from './components/ui/Components/select';
+
+
+import { Input } from './components/ui/Components/input';
+
+
+// 1️⃣ SelectTrigger → 크기, 보더, 배경색 변경
+// 2️⃣ SelectContent → 드롭다운의 테두리, 배경, 애니메이션 변경
+// 3️⃣ SelectItem → 선택된 항목 강조, hover 효과 추가
+
 export default function Test() {
   return (
     <div className="max-w-md mx-auto flex flex-col items-center">
 
 
+      <Select>
+        <SelectTrigger className="w-[180px]  border-purple-400  border-2">
+          <SelectValue placeholder="Theme" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="light">Light</SelectItem>
+          <SelectItem value="dark">Dark</SelectItem>
+          <SelectItem value="system">System</SelectItem>
+        </SelectContent>
+      </Select>
 
-
+<Input/>
     </div>
   );
 }

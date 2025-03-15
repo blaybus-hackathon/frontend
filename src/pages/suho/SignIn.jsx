530164d947001aae0ff276a5399d65feeb01f1b2
zhzkal2 <reliya1541@gmail.com>
2025-03-15 21:51:53 +0900

    feat : SignIn page update

--- src/pages/suho/SignIn.jsx
+++ src/pages/suho/SignIn.jsx
@@ -44,7 +44,7 @@ export default function SignIn() {
     return (
         <>
 
-            <div>Sign in</div>
+            
             <div className="min-h-screen flex flex-col">
 
                 <main className="flex-grow ">

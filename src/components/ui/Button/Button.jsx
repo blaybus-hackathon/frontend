2f826ae0da59ffe249344e9f3a06e6a1b08bc716
bigmooon <bigmooon1215@gmail.com>
2025-03-17 18:55:08 +0900

    fix : add hover to button - outline component

--- src/components/ui/Button/Button.jsx
+++ src/components/ui/Button/Button.jsx
@@ -4,7 +4,7 @@ import { cva } from 'class-variance-authority';
 import { cn } from '@/lib/utils';
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-[0.88rem] w-[88%] mb-[1.94rem] whitespace-nowrap transition-colors rounded-[0.625rem] text-xl font-bold disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 [&_svg]:shrink-0 ring-ring/10 dark:ring-ring/20 dark:outline-ring/40 outline-ring/50 focus-visible:ring-4 focus-visible:outline-1 aria-invalid:focus-visible:ring-0 ",
+  "inline-flex items-center justify-center w-[88%]  mb-[1.94rem] whitespace-nowrap transition-colors rounded-[0.625rem] text-xl font-bold disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 [&_svg]:shrink-0 ring-ring/10 dark:ring-ring/20 dark:outline-ring/40 outline-ring/50 focus-visible:ring-4 focus-visible:outline-1 aria-invalid:focus-visible:ring-0 ",
   {
     variants: {
       variant: {
@@ -13,7 +13,8 @@ const buttonVariants = cva(
         white:
           'bg-white text-[var(--main)] border border-[var(--main)] active:bg-[var(--main)] active:text-white hover:bg-[var(--main)] hover:text-white',
         disabled: 'bg-[var(--disabled)] text-white border border-[var(--disabled)]',
-        outline: 'bg-white text-[var(--black)] border border-[var(--outline)] font-normal',
+        outline:
+          'bg-white text-[var(--black)] border border-[var(--outline)] font-normal hover:bg-[var(--main)] hover:text-white',
         ghost: 'bg-transparent hover:bg-accent hover:text-accent-foreground',
         link: 'text-primary underline-offset-4 hover:underline',
       },

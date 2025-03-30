8555b0214961a3fa6f30afef46bf791b0900891a
zhzkal2 <reliya1541@gmail.com>
2025-03-31 04:40:49 +0900

    feat : Account page

--- src/pages/helper/Account.jsx
+++ src/pages/helper/Account.jsx
@@ -1,27 +1,26 @@
+// ✅ 1. 외부 라이브러리 (React 및 패키지)
 import { useNavigate } from "react-router-dom";
 
+// ✅ 2. 상태 관리 (스토어, 컨텍스트 등)
+import { CARE_TYPES } from "@/store/suho/useCareTypeStore";
+import useAccountStore from "@/store/suho/useAccountStore";
 
-import Header from '@/components/ui/temp/Header';
-
-import { TextAreaInput } from '@/components/ui/TextAreaInput';
-import useAccountStore from '@/store/suho/useAccountStore';
-import { CARE_TYPES } from '@/store/suho/useCareTypeStore'; // 레이블 변환용으로만 사용
-
-import Header from "@/components/ui/temp/Header";
+// ✅ 3. UI 컴포넌트 (공통 UI → 커스텀 컴포넌트 순)
+import { TextAreaInput } from "@/components/ui/TextAreaInput";
 import { Button } from "@/components/ui/custom/Button";
 import { SectionTitle } from "@/components/ui/custom/SectionTitle";
 import { Input } from "@/components/ui/custom/input";
 import { Radio, RadioItem } from "@/components/ui/custom/multiRadio";
-import useAccountStore from "@/store/suho/useAccountStore";
 
-import { CARE_TYPES } from "@/store/suho/useCareTypeStore"; // 레이블 변환용으로만 사용
-
-
+// ✅ 4. 레이아웃 컴포넌트 (Header, Footer 등)
+import Header from "@/components/ui/temp/Header";
 import Footer from "@/components/ui/temp/Footer";
-import { TextAreaInput } from "@/components/ui/TextAreaInput";
 
-// img
-import location from '@/assets/images/location.png';
+// ✅ 5. 이미지 및 정적 파일
+import location from "@/assets/images/location.png";
+import overview from "@/assets/images/overview.png";
+import backarrow from "@/assets/images/back-arrow.png";
+import homecontrols from "@/assets/images/home-controls.png";
 
 export default function Account() {
   const navigate = useNavigate();
@@ -49,179 +48,145 @@ export default function Account() {
     });
   };
 
-  //TODO 시멘틱코드 의식할것. 
-  //TODO 프리티어 설정에서 tailwind 플러그인 도입할것것
+  //TODO (3) : 이미지 업로드 기능
+  //TODO (1) : 시멘틱코드 의식할것.
+  //TODO (1) : 프리티어 설정에서 tailwind 플러그인 도입할것것
+  //TODO (1) : section이 중복되므로 컴포넌트로 만들어 중복된 코드 작성 회피 필요.
   return (
     <>
-      <main className="max-w-md mx-auto flex flex-col justify-center items-center gap-4 p-4">
-        <Header title="프로필" />
+      <main className="max-w-md mx-auto flex flex-col justify-center gap-6 p-4">
+        <Header title="나의 계정" />
 
         {/* 기본 정보 */}
-
-        <profile className='space-y-4'>
+        <section className="flex flex-col gap-6">
           {/* 프로필 이미지 */}
-          <section className='flex items-center'>
-            <div className='flex flex-row justify-between items-center gap-12  h-auto pr-6 pl-6'>
+          <section className="flex items-center">
+            <div className="flex flex-row justify-between items-center gap-12  h-auto pr-6 pl-6">
               <img
-                src='/defaultProfile.png'
-                alt='프로필 이미지'
-                className='w-24 h-24 rounded-full '
+                src="/defaultProfile.png"
+                alt="프로필 이미지"
+                className="w-24 h-24 rounded-full "
               />
-              <div className='flex flex-col justify-center gap-2 text-left'>
-                <span>{profile.name || '홍길동'}</span>
+              <div className="flex flex-col justify-center gap-2 text-left">
+                <span>{profile.name || "홍길동"}</span>
 
                 <p>서울특별시 용산구 거주</p>
               </div>
             </div>
           </section>
 
-
-{/* 자기 소개 섹션 */}
-          <section className='space-y-2-'>
-
-            <div className='flex flex-col pr-6 pl-6 gap-2'>
-              <span className='text-left font-bold'>자기소개</span>
+          {/* 자기 소개 섹션 */}
+          <section className="space-y-2">
+            <div className="flex flex-col pr-6 pl-6 gap-2">
+              <span className="text-left font-bold">자기소개</span>
               {/* 내용은 zustand에저장한 내용 가져올 것 */}
-              <p className='border-1 rounded-2xl text-left p-4'> 한 사람,한 사람의 필요에 맞춰 따뜻하고 세심한 돌봄을 제공하는 요양사입니다</p>
-              
+              <p className="border-1 rounded-xl text-left p-4">
+                한 사람,한 사람의 필요에 맞춰 따뜻하고 세심한 돌봄을 제공하는
+                요양사입니다
+              </p>
             </div>
           </section>
 
-{/* 간병 경력 섹션 */}
-          <section className='space-y-2-'>
+          {/* 간병 경력 섹션 */}
+          <section className="space-y-2">
+            <div className="flex flex-col pr-6 pl-6 gap-2">
+              <span className="text-left font-bold">간병경력</span>
+              <p className="border-2 rounded-xl text-center p-3">신입</p>
+            </div>
+          </section>
 
-<div className='flex flex-col pr-6 pl-6 gap-2'>
-  <span className='text-left font-bold'>간병경력</span>
-<p className='border-2 rounded-2xl text-center p-3'>신입</p>
-  
-</div>
-</section>
+          {/* 선호 지역 섹션 */}
+          <section className="space-y-2 flex flex-col pr-6 pl-6 gap-2">
+            <span className="text-left font-bold">선호지역</span>
 
-{/* 선호 지역 섹션 */}
-<section className='space-y-2-'>
-
-<div className='flex flex-col pr-6 pl-6 gap-2'>
-  <span className='text-left font-bold'>선호지역</span>
-  <div className='flex flex-row border-2 rounded-2xl items-center p-3 gap-2' >
-    <img className='w-[24px] h-[24px] ' src={location} alt="" />
-    <p className='text-left'>서울 > 서울 전체</p>
-    </div>
-  
-</div>
-</section>
-
-{/* 나의 근무 가능 일정 섹션션 */}
-{/* 피그마 아이콘 변경 요청 */}
-
-<section className='space-y-2-'>
-
-<div className='flex flex-col pr-6 pl-6 gap-2'>
-  <span className='text-left font-bold'>나의 근무 가능 일정</span>
-  <div className='flex flex-row border-2 rounded-2xl items-center p-3 gap-2' >
-    <img className='w-[24px] h-[24px] ' src={location} alt="" />
-    <p className='text-left'>서울 > 서울 전체</p>
-    </div>
-  
-</div>
-</section>
-
-
-          <section className='space-y-2'>
-          <p>
-              <span className='text-gray-500'>연락처: </span>
-              {profile.phone || '미설정'}
+            <p className="text-left flex flex-row items-center gap-3 p-3 border-2 rounded-xl">
+              <img
+                className="w-[24px] h-[24px] "
+                src={location}
+                alt="location_icon"
+              />
+              서울
+              <img
+                src={backarrow}
+                alt="backarrow"
+                className="w-4 h-4 rotate-180"
+              />
+              서울 전체
             </p>
           </section>
-        </profile>
 
+          {/* 나의 근무 가능 일정 섹션 */}
+          {/* TODO : 피그마 아이콘 변경 */}
+          <section className="space-y-2 flex flex-col pr-6 pl-6 gap-2">
+            <span className="text-left font-bold">나의 근무 가능 일정</span>
+            <p className="text-left flex flex-row items-center gap-3 p-3 border-2 rounded-xl">
+              <img
+                className="w-[24px] h-[24px] "
+                src={location}
+                alt="location_icon"
+              />
+              월, 화, 수
+              <img
+                src={backarrow}
+                alt="backarrow"
+                className="w-4 h-4 rotate-180"
+              />
+              13:00 ~ 20:00
+            </p>
+          </section>
 
-{/* <SectionTitle title="간병경력이 있으신가요?" subtitle="선택 " /> */}
-{/* <SectionTitle
-            title="자기소개"
-            subtitle="선택 (보호자에게 노출됩니다.)"
-          /> */}
-
-
-        {/* 희망 급여 섹션 */}
-        <section className="space-y-4">
-          <h2 className="font-semibold">희망 급여</h2>
-          <div>
-            {profile.pay ? (
-              <p>
+          {/* 급여 섹션 */}
+          <section className="space-y-2 flex flex-col pr-6 pl-6 gap-2">
+            <span className="text-left font-bold">나의 희망급여</span>
+            <p className="text-left flex flex-row items-center gap-4 border-2 rounded-xl p-3">
+              <img
+                className="w-[24px] h-[24px]"
+                src={overview}
+                alt="overview_icon"
+              />
+              <span>
                 {profile.pay.type === "hourly"
                   ? "시급"
                   : profile.pay.type === "daily"
                   ? "일급"
                   : "주급"}
-                {profile.pay.amount?.toLocaleString()}원
-              </p>
-            ) : (
-              <p>설정된 급여가 없습니다.</p>
-            )}
-          </div>
+              </span>
+              <img
+                src={backarrow}
+                alt="backarrow"
+                className="w-4 h-4 rotate-180"
+              />
+              <span className="">{profile.pay.amount?.toLocaleString()}원</span>
+            </p>
+          </section>
+
+          {/* 돌봄 유형 섹션*/}
+          <section className="space-y-2 flex flex-col pr-6 pl-6 gap-2">
+            <span className="text-left font-bold">나의 희망 돌봄유형</span>
+            <p className="text-left flex flex-row items-center gap-4 border-2 rounded-xl p-3">
+              <img
+                className="w-[24px] h-[24px]"
+                src={homecontrols}
+                alt="homeControls_icon"
+              />
+              <span className="">{getCareTypeLabel("workTypes")}</span>
+            </p>
+          </section>
+
+          {/* 자격증 등록 섹션*/}
+          <section className="space-y-2 flex flex-col pr-6 pl-6 gap-2">
+            <span className="text-left font-bold">나의 소지 자격증</span>
+            <p className="text-left flex flex-row items-center gap-4 border-2 rounded-xl p-3">
+              <span className="">요양보호사</span>
+            </p>
+          </section>
         </section>
-
-        
-
-        {/* 돌봄 유형 섹션 */}
-        <section className="space-y-4">
-          <h2 className="font-semibold">돌봄 유형</h2>
-          <div className="space-y-2 text-sm">
-            <p>
-              <span className="text-gray-500">근무 종류: </span>
-              {getCareTypeLabel("workTypes")}
-            </p>
-            <p>
-              <span className="text-gray-500">장기요양등급: </span>
-              {getCareTypeLabel("careGrade")}
-            </p>
-
-            <p>
-              <span className="text-gray-500">동거인 여부: </span>
-              {getCareTypeLabel("livingArrangement")}
-            </p>
-            <p>{getCareTypeLabel("gender")}</p>
-            <p>
-              <span className="text-gray-500">식사보조: </span>
-              {getCareTypeLabel("mealCare")}
-            </p>
-            <p>
-              <span className="text-gray-500">이동보조: </span>
-              {getCareTypeLabel("mobilitySupport")}
-            </p>
-            <p>
-              <span className="text-gray-500">일상생활: </span>
-              {getCareTypeLabel("dailyLife")}
-            </p>
-          </div>
-        </section>
-
-        {/* 선호 지역 섹션 */}
-        <section className="space-y-4">
-          <h2 className="font-semibold">선호 지역</h2>
-          <div>
-            {Object.entries(profile.locations || {})
-              .filter(([_, districts]) => districts.length > 0)
-              .map(([city, districts]) =>
-                districts.map((district) => `${city}-${district}`)
-              )
-              .flat()
-              .join(", ") || "선택된 지역이 없습니다."}
-          </div>
-        </section>
-
-        {/* 근무 가능 시간 섹션 */}
-        <section className="space-y-4">
-          <h2 className="font-semibold">근무 가능 시간</h2>
-          <div>
-            {Object.entries(profile.schedules || {})
-              .map(([day, time]) => `${day}요일 ${time.start}시-${time.end}시`)
-              .join(", ") || "설정된 근무시간이 없습니다."}
-          </div>
-        </section>
-
-        <Button variant="default" onClick={handleEdit} className="mt-4 ">
-          프로필 수정하기
+        <Button
+          variant="default"
+          onClick={handleEdit}
+          className="mt-6 mr-6 ml-6 "
+        >
+          수정하기
         </Button>
       </main>
       <Footer />

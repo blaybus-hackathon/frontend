12bd5fff7baef6bbcff50f2f88023d97e4da3896
zhzkal2 <reliya1541@gmail.com>
2025-04-09 01:29:37 +0900

    fix : consol.log

--- src/pages/helper/AccountEdit/CareTypeSection .jsx
+++ src/pages/helper/AccountEdit/CareTypeSection .jsx
@@ -6,7 +6,6 @@ const CareTypeSection = () => {
   const navigate = useNavigate();
   const { profileEdit } = useProfileStore(); // Zustand store에서 데이터 가져오기
   const careTypeData = profileEdit.careTypes;
-  // console.log(careTypeData);
 
   return (
     <section

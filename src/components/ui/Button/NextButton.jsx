3e8a756a65b8a0a747aaaa0d836fd12a4ad91b00
bigmooon <bigmooon1215@gmail.com>
2025-04-11 00:12:57 +0900

    add elderBasicInfo components

---
+++ src/components/ui/Button/NextButton.jsx
@@ -0,0 +1,23 @@
+import { Button } from '@/components/ui/custom/button';
+import { useElderRegiStepStore } from '@/store/center/useElderRegiStepStore.js';
+
+export default function NextButton({ className, disabled, onClick }) {
+  const nextStep = useElderRegiStepStore((state) => state.nextStep);
+
+  const handleClick = () => {
+    if (!disabled) {
+      nextStep();
+      onClick?.();
+    }
+  };
+
+  return (
+    <Button
+      className={`w-[100%] h-[4.0625rem] ${disabled ? 'bg-[#B8B8B8] cursor-not-allowed' : ''} ${className}`}
+      onClick={handleClick}
+      disabled={disabled}
+    >
+      다음
+    </Button>
+  );
+}

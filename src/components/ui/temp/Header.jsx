05ad0a16dc9d994e8d35caee8819501950d7d32c
bigmooon <bigmooon1215@gmail.com>
2025-04-20 22:57:52 +0900

    feat : add variant-based conditional rendering at Header

--- src/components/ui/temp/Header.jsx
+++ src/components/ui/temp/Header.jsx
@@ -1,13 +1,9 @@
 import { useNavigate } from 'react-router-dom';
 import backBtn from '@/assets/images/back-arrow.png';
 import Progress from './Progress';
+import Logo from '@/assets/images/logo.svg';
 
-export default function Header({ title }) {
-  const navigate = useNavigate();
-
-  const handleBack = () => {
-    navigate(-1);
-  };
+const DefaultHeader = ({ title, handleBack }) => {
   return (
     <header className='flex items-center px-6 h-[3.375rem] justify-between border-b border-b-[#C8C8C8]'>
       <button className='w-[1.625rem] h-auto cursor-pointer' onClick={handleBack}>
@@ -17,4 +13,28 @@ export default function Header({ title }) {
       <Progress currentPage={1} TotalPages={5} />
     </header>
   );
+};
+
+const SimpleHeader = () => {
+  return (
+    <header className='flex items-center px-[1.75rem] h-[5.625rem] justify-start'>
+      <img src={Logo} alt='logo' className='w-[49%]' />
+    </header>
+  );
+};
+
+export default function Header({ title, variant = 'default' }) {
+  const navigate = useNavigate();
+
+  const handleBack = () => {
+    navigate(-1);
+  };
+
+  switch (variant) {
+    case 'simple':
+      return <SimpleHeader />;
+    case 'default':
+    default:
+      return <DefaultHeader title={title} handleBack={handleBack} />;
+  }
 }

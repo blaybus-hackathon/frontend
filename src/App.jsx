80e34e3cd323443588d4b825025f0585e132d286
bigmooon <bigmooon1215@gmail.com>
2025-05-18 00:50:06 +0900

    refactor: restructure layout in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Layout from './components/ui/Layout';
 
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
@@ -31,34 +32,37 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Home />} />
         {/* 공통 */}
         <Route path='/signIn' element={<SignIn />} />
         <Route path='/signUp' element={<SignUp />} />
 
-        {/* Center */}
-        <Route path='/center/register' element={<ElderRegister />} />
-        <Route path='/center/mypage' element={<MyPage />} />
-        <Route path='/center/matching-info' element={<MatchingInfo />} />
-        <Route path='center/register/address' element={<ElderAddress />} />
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Home />} />
 
-        {/* Helper */}
-        <Route path='/helper/account' element={<Account />} />
-        <Route path='/helper/account/edit' element={<AccountEdit />} />
-        <Route path='/helper/account/schedule' element={<AccountSchedule />} />
-        <Route path='/helper/account/pay' element={<AccountPay />} />
-        <Route path='/helper/account/care-type' element={<AccountCareType />} />
-        <Route path='/helper/location' element={<HelperLocation />} />
-        <Route path='/helper/location/:city' element={<HelperLocationDetail />} />
+          {/* Center */}
+          <Route path='center/register' element={<ElderRegister />} />
+          <Route path='center/mypage' element={<MyPage />} />
+          <Route path='center/matching-info' element={<MatchingInfo />} />
+          <Route path='center/register/address' element={<ElderAddress />} />
 
-        <Route path='/matching' element={<Matching />} />
-        <Route path='/status' element={<MatchingStatus />} />
-        <Route path='/modify' element={<ModifyInfo />} />
-        <Route path='/caredetail' element={<CaregiverInfo />} />
-        <Route path='/test' element={<SignTest />} />
+          {/* Helper */}
+          <Route path='helper/account' element={<Account />} />
+          <Route path='helper/account/edit' element={<AccountEdit />} />
+          <Route path='helper/account/schedule' element={<AccountSchedule />} />
+          <Route path='helper/account/pay' element={<AccountPay />} />
+          <Route path='helper/account/care-type' element={<AccountCareType />} />
+          <Route path='helper/location' element={<HelperLocation />} />
+          <Route path='helper/location/:city' element={<HelperLocationDetail />} />
 
-        <Route path='/chatrooms' element={<ChatRoomsPage />} />
-        <Route path='/chatroom/:roomid' element={<PrivateChatRoom />} />
+          <Route path='matching' element={<Matching />} />
+          <Route path='status' element={<MatchingStatus />} />
+          <Route path='modify' element={<ModifyInfo />} />
+          <Route path='caredetail' element={<CaregiverInfo />} />
+          <Route path='test' element={<SignTest />} />
+
+          <Route path='chatrooms' element={<ChatRoomsPage />} />
+          <Route path='chatroom/:roomid' element={<PrivateChatRoom />} />
+        </Route>
       </Routes>
     </Router>
   );

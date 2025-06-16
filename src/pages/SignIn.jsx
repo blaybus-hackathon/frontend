d48c817df1f48a1c81ba2ccf653b2843d84574dd
bigmooon <bigmooon1215@gmail.com>
2025-06-16 20:08:56 +0900

    feat: add UI to SignIn page

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,12 +1,12 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import AuthForm from "@/components/Auth/Authform";
-import axios from "axios";
-import useProfileStore from "@/store/useProfileStore";
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import AuthForm from '@/components/Auth/Authform';
+import axios from 'axios';
+import useProfileStore from '@/store/useProfileStore';
 
 export default function SignIn() {
   const navigate = useNavigate();
-  const [loginType, setLoginType] = useState("helper");
+  const [loginType, setLoginType] = useState('helper');
   const { updateAuth } = useProfileStore();
 
   const handleSubmit = async ({ email, password }) => {
@@ -14,18 +14,18 @@ export default function SignIn() {
       console.log(email, password);
 
       const response = await axios.post(
-        "http://localhost:8080/api/sign/in",
+        'http://localhost:8080/api/sign/in',
         {
           userId: email,
           userPw: password,
         },
         {
           withCredentials: true,
-        }
+        },
       );
 
       if (response.status === 200) {
-        console.log("로그인 성공 ", response);
+        console.log('로그인 성공 ', response);
         //parse
         const { chatSenderId, email, userAuth } = response.data;
         //Zustand에 저장
@@ -35,29 +35,21 @@ export default function SignIn() {
           userAuth,
         });
         //zustand 프로필 내용도 저장 (세션에)
-        navigate("/");
+        navigate('/');
       }
     } catch (error) {
-      console.error("로그인 실패:", error);
-      alert("이메일 또는 비밀번호가 올바르지 않습니다.");
+      console.error('로그인 실패:', error);
+      alert('이메일 또는 비밀번호가 올바르지 않습니다.');
     }
   };
 
   return (
-    <>
-      <div className="min-h-screen flex flex-col">
-        <main className="flex-grow ">
-          <div className="container mx-auto px-4 ">
-            <div className="h-[800px] flex items-center justify-center">
-              <AuthForm
-                type={loginType}
-                onSubmit={handleSubmit}
-                setLoginType={setLoginType}
-              />
-            </div>
-          </div>
-        </main>
+    <main className='flex h-screen items-center'>
+      <div className='container mx-auto'>
+        <div className='flex'>
+          <AuthForm type={loginType} onSubmit={handleSubmit} setLoginType={setLoginType} />
+        </div>
       </div>
-    </>
+    </main>
   );
 }

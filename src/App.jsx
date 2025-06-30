6f6b735f2dc935b1622431e450996acee2dca216
bigmooon <bigmooon1215@gmail.com>
2025-06-30 23:25:06 +0900

    chore: delete ModifyInfo in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,6 @@ import AccountCareType from './pages/helper/AccountEdit/navigate/AccountCareType
 import Home from './pages/Home';
 import Matching from './pages/center/Matching';
 import RecriutDetail from './pages/center/RecriutDetail';
-import ModifyInfo from './pages/center/ModifyInfo';
 import CaregiverInfo from './pages/center/CaregiverInfo';
 
 import CenterRegister from './pages/center/CenterRegister';
@@ -38,6 +37,7 @@ function App() {
         {/* 공통 */}
         <Route index element={<Home />} />
         <Route path='/oauth/kakao/callback' element={<KakaoCallback />} />
+        <Route path='/center/matching-info' element={<MatchingInfo />} />
         <Route path='/*' element={<NotFound />} />
 
         {/* 레이아웃 */}
@@ -53,8 +53,6 @@ function App() {
           <Route path='center/elder-info' element={<ElderInfo />} />
           <Route path='center/matching' element={<Matching />} />
           <Route path='center/recruit-detail' element={<RecriutDetail />} />
-          <Route path='center/modify-info' element={<ModifyInfo />} />
-          <Route path='center/matching-info' element={<MatchingInfo />} />
           <Route path='center/caregiver-info' element={<CaregiverInfo />} />
           <Route path='center/register/address' element={<ElderAddress />} />
 

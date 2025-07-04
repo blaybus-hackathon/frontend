a8968d2daeb0384f588beb225f259d14508c29bd
bigmooon <bigmooon1215@gmail.com>
2025-07-04 18:32:17 +0900

    chore:delete unnecessary files

--- src/components/ui/custom/MatchingCard/MatchingCard.jsx
+++
@@ -1,7 +0,0 @@
-export default function MatchingCard({ children }) {
-  return (
-    <div className='flex flex-col w-full rounded-[0.625rem] border-2 border-[var(--outline)] justify-center items-center pt-[1.59rem] pb-[1.19rem] lg:h-[50rem] lg:justify-start'>
-      <div>{children}</div>
-    </div>
-  );
-}

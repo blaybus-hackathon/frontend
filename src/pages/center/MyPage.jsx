34730d3f796443304f131de61039fbff9b83fe23
bigmooon <bigmooon1215@gmail.com>
2025-07-23 00:06:50 +0900

    refactor: refactored MyPage

--- src/pages/center/MyPage.jsx
+++ src/pages/center/MyPage.jsx
@@ -18,7 +18,6 @@ export default function MyPage() {
   useEffect(() => {
     setHeaderProps({
       type: 'logo',
-      hasBorder: false,
     });
 
     return () => {
@@ -27,11 +26,14 @@ export default function MyPage() {
   }, [clearHeaderProps, setHeaderProps]);
 
   // TODO: 로딩 컴포넌트 필요
-  if (isLoading) return <div>Loading...</div>;
-  if (!managerProfile) {
-    alert('관리자 정보를 불러오지 못했습니다.');
-    navigate('/signin');
-  }
+  useEffect(() => {
+    if (!isLoading && !managerProfile) {
+      alert('관리자 정보를 불러오지 못했습니다.');
+      navigate('/signin');
+    }
+  }, [isLoading, managerProfile, navigate]);
+
+  if (isLoading || !managerProfile) return <div>Loading...</div>; //
 
   // save formData when edit mode is true
   const handleEditClick = () => {
@@ -58,45 +60,54 @@ export default function MyPage() {
     }
   };
 
-  return (
-    <div className='h-screen max-w-2xl mx-auto'>
-      {/* 헤더 밑 공간 */}
-      <div className='pt-[1.88rem] bg-[#f5f5f5]'>
-        {/* 마이페이지 전체 컨테이너 */}
-        <div className='flex items-center justify-between bg-white'>
-          {/* 하단 넓이 */}
-          <div className='w-[88%] mx-auto'>
-            {/* 프로필 영역 */}
-            <seciton className='flex items-center w-full gap-[1.5rem] py-[2rem]'>
-              <img src={Profile} alt='profile' className='w-[6.56rem] h-[6.56rem]' />
-              <div className='flex flex-col items-start'>
-                <p className='text-[1.43rem] font-bold text-[var(--black)]'>
-                  {managerProfile.cmName} {managerProfile.cmPosition}
-                </p>
-                <p className='text-[1.25rem] font-medium text-[var(--black)]'>
-                  {managerProfile.centerName}
-                </p>
-              </div>
-            </seciton>
-            {isEditMode ? (
-              <ManagerInfoEdit />
-            ) : (
-              <ManagerInfoDisplay managerProfile={managerProfile} />
-            )}
+  const getImgUrl = (imgAddress) => {
+    if (!imgAddress) return null;
 
-            <section className='flex flex-col items-center mb-[2.6rem]'>
-              {isEditMode || (
-                <Button variant='white' onClick={handleEditClick} className={'w-full mb-5'}>
-                  로그아웃
-                </Button>
-              )}
-              <Button onClick={isEditMode ? handleSubmit : handleEditClick} className={'w-full'}>
-                {isEditMode ? '저장하기' : '수정하기'}
-              </Button>
-            </section>
+    const fileName = imgAddress.split('\\').pop();
+    return fileName;
+  };
+
+  return (
+    <article className='flex flex-col'>
+      {/* 마이페이지 전체 컨테이너 */}
+      <div className='flex flex-col items-center justify-between'>
+        {/* 프로필 영역 */}
+        <section className='flex items-center w-full gap-[1.5rem] py-[2rem]'>
+          <img
+            src={getImgUrl(managerProfile.imgAddress) || Profile}
+            // src={managerProfile.imgAddress || Profile}
+            alt='profile'
+            className='w-[6.56rem] h-[6.56rem]'
+          />
+          <div className='flex flex-col items-start'>
+            <p className='text-[1.43rem] font-bold text-[var(--black)]'>
+              {managerProfile.cmName} {managerProfile.cmPosition}
+            </p>
+            <p className='text-[1.25rem] font-medium text-[var(--black)]'>
+              {managerProfile.centerName}
+            </p>
           </div>
-        </div>
+        </section>
+
+        <section className='w-full'>
+          {isEditMode ? (
+            <ManagerInfoEdit />
+          ) : (
+            <ManagerInfoDisplay managerProfile={managerProfile} />
+          )}
+        </section>
       </div>
-    </div>
+
+      <section className='flex flex-col items-center mb-[2.6rem]'>
+        {isEditMode || (
+          <Button variant='white' onClick={handleEditClick} className={'w-full mb-5'}>
+            로그아웃
+          </Button>
+        )}
+        <Button onClick={isEditMode ? handleSubmit : handleEditClick} className={'w-full'}>
+          {isEditMode ? '저장하기' : '수정하기'}
+        </Button>
+      </section>
+    </article>
   );
 }

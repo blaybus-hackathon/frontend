8978d8378f5f61ec763ef966ddc4f8762692031f
bigmooon <bigmooon1215@gmail.com>
2025-07-30 20:09:18 +0900

    feat: add HelperHome/Request page

--- src/components/helper/Matching/MatchingRequest.jsx
+++ src/components/helper/Matching/MatchingRequest.jsx
@@ -2,6 +2,7 @@ import { Card, CardContent } from '@/components/ui/card';
 import ElderProfile from '@/components/ui/InfoCard/ElderProfile';
 import InfoField from '@/components/ui/InfoCard/InfoField';
 import { Button } from '@/components/ui/custom/Button';
+import { formatAddress } from '@/utils/formatters/formatAddress';
 
 export default function MatchingRequest({ data = [], onDetail }) {
   if (!data || data.length === 0) {
@@ -10,16 +11,19 @@ export default function MatchingRequest({ data = [], onDetail }) {
 
   return (
     <>
-      <section className='flex flex-col mx-auto gap-5 mt-5'>
+      <section className='w-[88%] lg:w-[95%] mx-auto flex flex-col gap-5 mt-5'>
         {data.map((req) => (
           <Card key={req.patientSeq} className='px-5 border-2'>
             <ElderProfile elderInfo={req} detailInfo={false} className='pb-0' />
             <hr />
             <CardContent className='grid gap-y-5 px-0'>
-              <InfoField label='근무종류' text={req.workTypeText} />
-              <InfoField label='주소지' text={req.fullAddress} />
+              <InfoField label='근무종류' text={req.workType} />
+              <InfoField
+                label='주소지'
+                text={formatAddress(req.addressFirst, req.addressSecond, req.addressThird)}
+              />
               <Button
-                className={`w-full text-lg font-semibold h-fit p-3`}
+                className={`w-full text-lg font-semibold h-fit p-3 lg:mt-5`}
                 onClick={() => onDetail(req)}
               >
                 자세히 보기
--- src/pages/helper/MatchingDetail.jsx
+++ src/pages/helper/MatchingDetail.jsx
@@ -3,10 +3,13 @@ import RequestDetail from '@/components/helper/Matching/RequestDetail';
 import CompleteDetail from '@/components/helper/Matching/CompleteDetail';
 
 export default function MatchingDetail() {
-  const patientSeq = useParams().patientSeq;
+  const patientLogSeq = useParams().patientLogSeq;
   const [searchParams] = useSearchParams();
   const status = searchParams.get('status'); // request, complete
+  const returnPath = searchParams.get('return');
 
-  if (status === 'request') return <RequestDetail patientSeq={patientSeq} />;
-  if (status === 'complete') return <CompleteDetail patientSeq={patientSeq} />;
+  if (status === 'request')
+    return <RequestDetail patientLogSeq={patientLogSeq} returnPath={returnPath} />;
+  if (status === 'complete')
+    return <CompleteDetail patientLogSeq={patientLogSeq} returnPath={returnPath} />;
 }

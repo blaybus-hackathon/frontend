3eaf8182844b68d07b32cede4a21b8a29cd85f82
bigmooon <bigmooon1215@gmail.com>
2025-07-30 20:09:40 +0900

    style: add style to InfoField

--- src/components/ui/InfoCard/InfoField.jsx
+++ src/components/ui/InfoCard/InfoField.jsx
@@ -4,7 +4,7 @@ export default function InfoField({ label = '', text = '' }) {
       <div className='grid grid-cols-[5rem_1fr] items-center gap-3 md:gap-16'>
         <h4 className='text-start text-[var(--black)] text-lg font-semibold'>{label}</h4>
         <div className='flex justify-start bg-[#f6f6f6] p-5 rounded-[0.625rem]'>
-          <span className='text-start text-[var(--black)] text-lg font-normal break-keep'>
+          <span className='text-start text-[var(--black)] text-lg font-normal break-keep whitespace-pre-wrap'>
             {text}
           </span>
         </div>

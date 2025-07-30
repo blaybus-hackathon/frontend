a3826c34d14a324239b067399999bf35b2ec647f
bigmooon <bigmooon1215@gmail.com>
2025-07-30 20:12:18 +0900

    chore: delete unneccesary line in MatcingCompleted and useMatchList in center folder

--- src/components/Center/MatchingInfo/MatchingCompleted.jsx
+++ src/components/Center/MatchingInfo/MatchingCompleted.jsx
@@ -5,7 +5,7 @@ import InfoCard from '@/components/ui/InfoCard/InfoCard';
 import ElderProfile from '@/components/ui/InfoCard/ElderProfile';
 import HelperProfile from '@/components/ui/InfoCard/HelperProfile';
 
-export default memo(function MatchingCompleted({ data }) {
+export default (function MatchingCompleted({ data }) {
   const fullAddress = formatAddress(data.addressFirst, data.addressSecond, data.addressThird);
   const helperList = data.matchFinHelperInfoList || [];
   const hasHelpers = helperList.length > 0;

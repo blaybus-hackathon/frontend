fd65fccca5a807275c88e83ec68d086749702581
bigmooon <bigmooon1215@gmail.com>
2025-08-14 18:37:50 +0900

    chore: move folder of validator/center/elderRegister into validator/center/elder

---
+++ src/utils/validators/center/elder/index.js
@@ -0,0 +1,5 @@
+// src/utils/validators/center/elder/index.js
+export { validateBasicInfo } from './basicInfo.js';
+export { validateCareInfo } from './careInfo.js';
+export { validateServiceInfo } from './serviceInfo.js';
+export { validateAddInfo } from './additionalInfo.js';

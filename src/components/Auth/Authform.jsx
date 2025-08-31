b07259bb6bfd99d9e97131f6aa54bcfc8e3fb471
bigmooon <bigmooon1215@gmail.com>
2025-08-31 18:04:16 +0900

    feat: add openid parameter to Authform

--- src/components/Auth/Authform.jsx
+++ src/components/Auth/Authform.jsx
@@ -15,7 +15,7 @@ const AuthForm = ({ type, onSubmit, setLoginType }) => {
 
   const REST_API_KEY = import.meta.env.VITE_KAKAO_REST_API_KEY;
   const REDIRECT_URI = import.meta.env.VITE_KAKAO_REDIRECT_URI;
-  const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code&scope=profile_nickname,profile_image,account_email`;
+  const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code&scope=profile_nickname,profile_image,account_email,openid`;
 
   function handleSubmit(e) {
     e.preventDefault();

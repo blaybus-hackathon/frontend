5011ff2d3fc39959900638c12f8b96a73fe0fb10
bigmooon <bigmooon1215@gmail.com>
2025-09-24 16:24:35 +0900

    chore: add prettier to Authform

--- src/components/Auth/Authform.jsx
+++ src/components/Auth/Authform.jsx
@@ -1,17 +1,17 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { Button } from "@/components/ui/custom/Button";
-import { Input } from "@/components/ui/custom/input";
-import { Alert } from "@/components/ui/custom/alert";
-import { User, LockKeyholeOpen } from "lucide-react";
-import { navigateToHome } from "@/routes/homeNavigation";
-import Kakao from "/kakao_login_medium_wide.svg";
-import Logo from "/blaybus_logo_icon_text.svg";
-import { CLIENT_ROLE, ROLE_MAP } from "@/constants/authType";
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Button } from '@/components/ui/custom/Button';
+import { Input } from '@/components/ui/custom/input';
+import { Alert } from '@/components/ui/custom/alert';
+import { User, LockKeyholeOpen } from 'lucide-react';
+import { navigateToHome } from '@/routes/homeNavigation';
+import Kakao from '/kakao_login_medium_wide.svg';
+import Logo from '/blaybus_logo_icon_text.svg';
+import { CLIENT_ROLE, ROLE_MAP } from '@/constants/authType';
 
 const AuthForm = ({ type, onSubmit, setLoginType }) => {
   const navigate = useNavigate();
-  const [alertMessage, setAlertMessage] = useState("");
+  const [alertMessage, setAlertMessage] = useState('');
 
   const REST_API_KEY = import.meta.env.VITE_REST_API_KEY;
   const REDIRECT_URI = import.meta.env.VITE_REDIRECT_URI;
@@ -19,52 +19,50 @@ const AuthForm = ({ type, onSubmit, setLoginType }) => {
 
   function handleSubmit(e) {
     e.preventDefault();
-    setAlertMessage("");
+    setAlertMessage('');
 
     const formData = new FormData(e.target);
-    const email = formData.get("email")?.trim();
-    const password = formData.get("password")?.trim();
+    const email = formData.get('email')?.trim();
+    const password = formData.get('password')?.trim();
 
     if (!email || !password) {
-      setAlertMessage("이메일과 비밀번호를 모두 입력해주세요.");
+      setAlertMessage('이메일과 비밀번호를 모두 입력해주세요.');
       return;
     }
     onSubmit({ email, password, loginType: type });
   }
 
   function handleSignUpNavigation() {
-    navigate(type === "helper" ? "/helper/signup" : "/center/signup");
+    navigate(type === 'helper' ? '/helper/signup' : '/center/signup');
   }
 
   function handleKakaoLogin() {
     const serverRole = ROLE_MAP.toServer[type]; // MANAGER | MEMBER
-    const redirectUri = encodeURIComponent(
-      "https://dolbom-work.co.kr/login/oauth2/code/kakao"
-    );
+    const redirectUri = encodeURIComponent('https://dolbom-work.co.kr/login/oauth2/code/kakao');
     window.location.href = `${KAKAO_AUTH_URL}&redirect_uri=${redirectUri}&state=${serverRole}`;
   }
   function handleFindAccount(e) {
     e.preventDefault();
-    navigate("/find-account");
+    navigate('/find-account');
   }
 
   return (
-    <div className="bg-white rounded-lg mx-auto">
+    <div className='bg-white rounded-lg mx-auto'>
       <button
         onClick={() => navigateToHome(navigate)}
-        className="w-1/2 mx-auto py-8 cursor-pointer"
+        className='w-1/2 mx-auto py-8 cursor-pointer'
       >
-        <img src={Logo} alt="blaybus logo" className="w-full" />
+        <img src={Logo} alt='blaybus logo' className='w-full' />
       </button>
 
       {/* select login type */}
-      <div className="grid grid-cols-2 mb-8 gap-4">
+      <div className='grid grid-cols-2 mb-8 gap-4'>
         <Button
           onClick={() => setLoginType(CLIENT_ROLE.CENTER)}
           className={`w-full h-[3.44rem] font-medium text-lg rounded-[0.31rem] border-none ${
             type === CLIENT_ROLE.CENTER
-              ? "bg-[var(--main)] text-white"
-              : "bg-[var(--button-inactive)] text-[var(--placeholder-gray)]"
+              ? 'bg-[var(--main)] text-white'
+              : 'bg-[var(--button-inactive)] text-[var(--placeholder-gray)]'
           }`}
         >
           관리자용
@@ -73,8 +71,8 @@ const AuthForm = ({ type, onSubmit, setLoginType }) => {
           onClick={() => setLoginType(CLIENT_ROLE.HELPER)}
           className={`w-full h-[3.44rem] font-medium text-lg rounded-[0.31rem] border-none ${
             type === CLIENT_ROLE.HELPER
-              ? "bg-[var(--main)] text-white"
-              : "bg-[var(--button-inactive)] text-[var(--placeholder-gray)]"
+              ? 'bg-[var(--main)] text-white'
+              : 'bg-[var(--button-inactive)] text-[var(--placeholder-gray)]'
           }`}
         >
           요양보호사용
@@ -82,41 +80,39 @@ const AuthForm = ({ type, onSubmit, setLoginType }) => {
       </div>
 
       {/* Alert 메시지 표시 */}
-      {alertMessage && (
-        <Alert description={alertMessage} color="red" className="mb-4" />
-      )}
+      {alertMessage && <Alert description={alertMessage} color='red' className='mb-4' />}
 
-      <form onSubmit={handleSubmit} className="space-y-4">
-        <div className="relative">
-          <User className="absolute left-5 top-1/2 -translate-y-1/2 h-5 w-5 text-[var(--placeholder-gray)] z-1" />
+      <form onSubmit={handleSubmit} className='space-y-4'>
+        <div className='relative'>
+          <User className='absolute left-5 top-1/2 -translate-y-1/2 h-5 w-5 text-[var(--placeholder-gray)] z-1' />
           <Input
-            id="email"
-            name="email"
-            type="email"
-            placeholder="아이디를 입력해주세요."
-            className="w-full h-12 pl-12 placeholder:text-[var(--placeholder-gray)]"
+            id='email'
+            name='email'
+            type='email'
+            placeholder='아이디를 입력해주세요.'
+            className='w-full h-12 pl-12 placeholder:text-[var(--placeholder-gray)]'
             required
-            autoComplete="email"
+            autoComplete='email'
           />
         </div>
 
-        <div className="relative">
-          <LockKeyholeOpen className="absolute left-5 top-1/2 -translate-y-1/2 h-5 w-5 text-[var(--placeholder-gray)] z-1" />
+        <div className='relative'>
+          <LockKeyholeOpen className='absolute left-5 top-1/2 -translate-y-1/2 h-5 w-5 text-[var(--placeholder-gray)] z-1' />
           <Input
-            id="password"
-            name="password"
-            type="password"
-            placeholder="비밀번호를 입력해주세요."
-            className="w-full h-12 pl-12 placeholder:text-[var(--placeholder-gray)]"
+            id='password'
+            name='password'
+            type='password'
+            placeholder='비밀번호를 입력해주세요.'
+            className='w-full h-12 pl-12 placeholder:text-[var(--placeholder-gray)]'
             required
-            autoComplete="current-password"
+            autoComplete='current-password'
           />
         </div>
 
-        <div className="flex justify-end mb-8">
+        <div className='flex justify-end mb-8'>
           <button
-            type="button"
-            className="text-base lg:text-[1.06rem] font-medium text-[var(--placeholder-gray)] border-b border-[var(--outline)] hover:bg-accent"
+            type='button'
+            className='text-base lg:text-[1.06rem] font-medium text-[var(--placeholder-gray)] border-b border-[var(--outline)] hover:bg-accent'
             onClick={handleFindAccount}
           >
             아이디 비밀번호 찾기
@@ -124,32 +120,30 @@ const AuthForm = ({ type, onSubmit, setLoginType }) => {
         </div>
 
         <Button
-          className={
-            "w-full h-[3.4rem] rounded-[0.31rem] text-base lg:text-lg font-medium"
-          }
-          type="submit"
+          className={'w-full h-[3.4rem] rounded-[0.31rem] text-base lg:text-lg font-medium'}
+          type='submit'
         >
           로그인
         </Button>
       </form>
 
       <Button
-        variant="white"
+        variant='white'
         onClick={handleSignUpNavigation}
         className={
-          "mt-2 w-full h-[3.4rem] rounded-[0.31rem] text-base lg:text-lg font-medium hover:brightness-90 transition-all"
+          'mt-2 w-full h-[3.4rem] rounded-[0.31rem] text-base lg:text-lg font-medium hover:brightness-90 transition-all'
         }
       >
         돌봄워크 회원가입
       </Button>
 
-      <div className="rounded-[0.31rem] overflow-hidden h-[3.4rem] w-full bg-[#fee500] flex items-center justify-center mt-2 cursor-pointer hover:brightness-90 transition-all">
+      <div className='rounded-[0.31rem] overflow-hidden h-[3.4rem] w-full bg-[#fee500] flex items-center justify-center mt-2 cursor-pointer hover:brightness-90 transition-all'>
         <button
-          className="h-full rounded-[0.31rem] overflow-hidden cursor-pointer"
+          className='h-full rounded-[0.31rem] overflow-hidden cursor-pointer'
           onClick={handleKakaoLogin}
-          type="button"
+          type='button'
         >
-          <img src={Kakao} alt="kakao" className="w-full h-full object-fit" />
+          <img src={Kakao} alt='kakao' className='w-full h-full object-fit' />
         </button>
       </div>
     </div>
